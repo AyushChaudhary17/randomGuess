0eb1f706c4ce3cf88c10292c6085099ffca6ea41
Ayush Chaudhary <ayushchaudhary1702@gmail.com>
2023-12-05 02:20:33 +0530

    added

---
+++ Bank/bank.java
@@ -0,0 +1,17 @@
+package Bank;
+/**
+ * bank
+ */
+class Account{
+    String name;
+    String number;
+    void display(String name, String number){
+        this.name=name;
+        this.number=number;
+        System.out.println("Account holder is "+this.name+"\nA/C number is "+this.number);
+    }
+}
+public class bank {
+
+    
+}
---
+++ Bank/user.java
@@ -0,0 +1,8 @@
+package Bank;
+
+public class user {
+    public static void main(String[] args){
+        Account user1=new Account();
+        user1.display("Ayush Chaudhary","13982612000248");
+    }
+}
